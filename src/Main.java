763a9f6aa975a3d63b324e83671cb19541c29aaf
kadvar <kadvar@yahoo.com>
2022-06-04 09:58:14 +0530

    added chart class.

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,37 @@ public class Main {
     public static void main(String[] args) {
         SwissEph sw = new SwissEph();
 
-        //Convert date to julDay
+        //Get today's date and convert to julDay
         SweDate sd = new SweDate();
 
-        //get planetary positions
-        double[] planet_lon = new double [6];
-        StringBuffer sb = new StringBuffer();
-        sw.swe_calc_ut(sd.getJulDay(), 1, SEFLG_TRUEPOS, planet_lon, sb);
+        //Testing getObjLon
+        double curr_jd = sd.getJulDay();
+        getObjLon(0, sd.getJulDay());
 
-        //Print the planet's lon
-        System.out.println(planet_lon[0] % 30);
+        //Print all planetary positions
+        for (int i=0; i<11; i++) {
+            double my_obj_lon = getObjLon(i, curr_jd) % 30;
+            System.out.println(sw.swe_get_planet_name(i)+" "+my_obj_lon);
+        }
     }
+
+    //Gets a specified object's longitude
+    private static double getObjLon(int obj, double jd) {
+
+        //Setup preparers for swe_calc_ut()
+        SwissEph sw = new SwissEph();
+        double[] obj_data = new double [6];
+        StringBuffer sb = new StringBuffer();
+
+        //calc position and store in obj_lon
+
+        sw.swe_calc_ut(jd, obj, SEFLG_TRUEPOS, obj_data, sb);
+//        sw.swe_set_sid_mode(SweConst.SE_SIDM_LAHIRI);
+//        sw.swe_calc_ut(jd, obj, SEFLG_SIDEREAL|SE_SPLIT_DEG_KEEP_SIGN, obj_data, sb);
+
+        //return obj lon only
+        //System.out.println("Returning obj lon: "+obj_data[0]);
+        return obj_data[0];
+    }
+
 }
