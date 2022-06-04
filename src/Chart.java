a48bdc19ffb307664e57e7b02b2a42128c86e375
kadvar <kadvar@yahoo.com>
2022-06-04 23:49:56 +0530

    chart class changes.

--- src/Chart.java
+++ src/Chart.java
@@ -2,9 +2,9 @@
  *This class represents a chart with
  * the planetary & house positions. */
 
-import swisseph.SweDate;
-import swisseph.SwissEph;
+import swisseph.*;
 
+import java.util.HashMap;
 import static swisseph.SweConst.SEFLG_TRUEPOS;
 
 public class Chart {
@@ -13,6 +13,8 @@ public class Chart {
     private double birthLon;
     private SweDate birthDate;
     public static SwissEph sw = new SwissEph();
+    private HashMap planetsHousesMap = new HashMap<>();
+    private SwissLib sl = new SwissLib();
 
     public double getBirthLat() {
         return birthLat;
@@ -46,6 +48,7 @@ public class Chart {
         this.birthName = birthName;
     }
 
+    /* Populates planet and house positions into hashmap */
     public void calcPlanetAndHousePositions() {
         //Setup preparers for swe_calc_ut()
         double[] od = new double[6];
@@ -56,12 +59,38 @@ public class Chart {
         double deltaT = birthDate.getDeltaT();
         double curr_jd = jd + deltaT;
 
-        //calc planetary positions and store in obj_lon
+        //calc planetary positions
         for (int i = 0; i < 11; i++) {
             sw.swe_calc_ut(jd, i, SEFLG_TRUEPOS, od, sb);
             double obj_lon = od[0];
-            //Store in hash
-            System.out.println(sw.swe_get_planet_name(i) + " " + obj_lon % 30);
+            //Store in HashMap
+            planetsHousesMap.put(sw.swe_get_planet_name(i), obj_lon);
+
+            IntObj ideg = new IntObj();
+            IntObj imin = new IntObj();
+            IntObj isec = new IntObj();
+            DblObj dsecfr = new DblObj();
+            IntObj isgn = new IntObj();
+
+            //Display in d,m,s
+            sl.swe_split_deg(obj_lon, SweConst.SE_SPLIT_DEG_ROUND_MIN|SweConst.SE_SPLIT_DEG_ZODIACAL, ideg, imin, isec, dsecfr, isgn);
+            System.out.println(sw.swe_get_planet_name(i)+" "+ideg.val+" "+imin.val+" "+isec.val+" sign"+isgn.val);
+
+        //calc house cusps
+            double[] house_cusps = new double[13];
+            double[] ascmc_data = new double[10];
+            sw.swe_houses(curr_jd, 0, birthLat, birthLon, 'P', house_cusps, ascmc_data);
+
+            //store asc and other results
+            double asc = ascmc_data[0];
+            double mc = ascmc_data[1];
+
+            //get all house cusps
+            for (int j = 1; j < 13; j++) {
+                //System.out.println("House " + j + ":" + house_cusps[j]);
+                planetsHousesMap.put("house"+(j), house_cusps[j]%30);
+            }
         }
+        System.out.println(planetsHousesMap);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,33 +3,14 @@ import static swisseph.SweConst.*;
 
 public class Main {
 
-
-    //    private static ArrayList<String> planetsList = new ArrayList<String>();
-//    public static void setPlanetsList() {
-//        planetsList.add("Sun");
-//        planetsList.add("Moon");
-//    }
-    //Set Latitude & Longitude
-    public static double place_lat = 12.9716;
-    public static double place_lon = 77.5946;
-
-    //Get today's date and convert to julday
-    static int hours = 14;
-    static double minutes = 46;
-    static SweDate sd = new SweDate(2022, 6, 4, hours + (minutes / 60), true);
-    public static double jd = sd.getJulDay();
-    public static double deltaT = sd.getDeltaT();
-    public static double curr_jd = jd + deltaT;
-
-    public static SwissEph sw = new SwissEph();
-
-    //Main method begins here
     public static void main(String[] args) {
 
         //Initialize a new chart object
         Chart chart1 = new Chart();
 
         //initialize parameters
+        int hours = 18;
+        double minutes = 15;
         SweDate sd = new SweDate(2022, 6, 4, hours + (minutes / 60), true);
         chart1.setBirthDate(sd);
         chart1.setBirthLon(77.5946);
@@ -38,37 +19,59 @@ public class Main {
 
         //populate planetary positions
         chart1.calcPlanetAndHousePositions();
+    }
+}
+
+    //    private static ArrayList<String> planetsList = new ArrayList<String>();
+//    public static void setPlanetsList() {
+//        planetsList.add("Sun");
+//        planetsList.add("Moon");
+//    }
+    //Set Latitude & Longitude
+//    public static double place_lat = 12.9716;
+//    public static double place_lon = 77.5946;
+
+    //Get today's date and convert to julday
+//    static int hours = 14;
+//    static double minutes = 46;
+//    static SweDate sd = new SweDate(2022, 6, 4, hours + (minutes / 60), true);
+//    public static double jd = sd.getJulDay();
+//    public static double deltaT = sd.getDeltaT();
+//    public static double curr_jd = jd + deltaT;
+//
+//    public static SwissEph sw = new SwissEph();
+
+    //Main method begins here
+
 
         //Calculate house positions
-        double[] house_cusps = new double[13];
-        double[] ascmc_data = new double[10];
-        sw.swe_houses(curr_jd, 0, place_lat, place_lon, 'P', house_cusps, ascmc_data);
-
-        //store asc and other results
-        double asc = ascmc_data[0];
-        double mc = ascmc_data[1];
-
-        //get house cusps
-        for (int i = 1; i < 13; i++) {
-            System.out.println("House " + i + ":" + house_cusps[i] % 30);
-        }
-    }
+//        double[] house_cusps = new double[13];
+//        double[] ascmc_data = new double[10];
+//        sw.swe_houses(curr_jd, 0, place_lat, place_lon, 'P', house_cusps, ascmc_data);
+//
+//        //store asc and other results
+//        double asc = ascmc_data[0];
+//        double mc = ascmc_data[1];
+//
+//        //get house cusps
+//        for (int i = 1; i < 13; i++) {
+//            System.out.println("House " + i + ":" + house_cusps[i] % 30);
+//        }
 
     //Gets a specified object's longitude
-    private static double getObjLon(int obj, double jd) {
-        //Setup preparers for swe_calc_ut()
-        double[] obj_data = new double[6];
-        StringBuffer sb = new StringBuffer();
-
-        //calc position and store in obj_lon
-        sw.swe_calc_ut(jd, obj, SEFLG_TRUEPOS, obj_data, sb);
-//        sw.swe_set_sid_mode(SweConst.SE_SIDM_LAHIRI);
-//        sw.swe_calc_ut(jd, obj, SEFLG_SIDEREAL|SE_SPLIT_DEG_KEEP_SIGN, obj_data, sb);
-        //return obj lon only
-        //System.out.println("Returning obj lon: "+obj_data[0]);
-        return obj_data[0];
-    }
-
+//    private static double getObjLon(int obj, double jd) {
+//        //Setup preparers for swe_calc_ut()
+//        double[] obj_data = new double[6];
+//        StringBuffer sb = new StringBuffer();
+//
+//        //calc position and store in obj_lon
+//        sw.swe_calc_ut(jd, obj, SEFLG_TRUEPOS, obj_data, sb);
+////        sw.swe_set_sid_mode(SweConst.SE_SIDM_LAHIRI);
+////        sw.swe_calc_ut(jd, obj, SEFLG_SIDEREAL|SE_SPLIT_DEG_KEEP_SIGN, obj_data, sb);
+//        //return obj lon only
+//        //System.out.println("Returning obj lon: "+obj_data[0]);
+//        return obj_data[0];
 
 
-}
+
+
